c83b922c20fcb76502405870aa5fa0d82cc54620
Ivan Nečas <inecas@redhat.com>
2014-08-28 22:35:33 +0200

    Foreman specific AppConfig

--- lib/hammer_cli_eval/context.rb
+++ lib/hammer_cli_eval/context.rb
@@ -6,12 +6,65 @@ module HammerCLIEval
       @hammer = hammer
     end
 
-    def api
-      @api ||= HammerCLI::Connection.get("foreman").api
+    class AppConfig < ApipieBindings::Model::AppConfig
+      def initialize
+        super(HammerCLI::Connection.get("foreman").api, "Foreman")
+      end
+
+      # TODO: ResoruceConfig
+
+      def sub_resources(resource_name, data)
+        case resource_name
+        when nil
+          [sub_resource(:organizations),
+           sub_resource(:hosts)]
+        when :organizations
+          [sub_resource(:products,
+                        { 'organization_id' => data['id'] })]
+        when :products
+          [sub_resource(:repositories,
+                        { 'organization_id' => data['organization_id'],
+                          'product_id'      => data['id']})]
+        else
+          []
+        end
+      end
+
+      def search_options(resource_name, conditions)
+        if scoped_search_resource?(resource_name)
+          query = conditions.map do |(key, value)|
+            "#{key} = \"#{value}\""
+          end.join(' AND ')
+          { :search => query }
+        else
+          super
+        end
+      end
+
+      def unique_keys(resoruce_name)
+        case resoruce_name
+        when :products
+          [%w[id],
+           %w[organization_id name],
+           %w[organization_id label]]
+        when :repositories
+          [%w[id],
+           %w[product_id name],
+           %w[product_id label]]
+        else
+          super
+        end
+      end
+
+      private
+
+      def scoped_search_resource?(name)
+        [:organizations].include?(name)
+      end
     end
 
     def foreman
-      @foreman ||= ApipieBindings::Model::App.new(api, 'Foreman')
+      @foreman ||= ApipieBindings::Model::App.new(AppConfig.new)
     end
 
     def pry
